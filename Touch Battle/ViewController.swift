e5923d01cbc7dc9bc46d8f35a3a05ecc98409a3c
PolMrt <pol@marnette.be>
2017-05-15 17:54:22 +0200

    Initial Commit

---
+++ Touch Battle/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Touch Battle
+//
+//  Created by Pol Marnette on 15/05/17.
+//  Copyright © 2017 Pol Marnette. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
