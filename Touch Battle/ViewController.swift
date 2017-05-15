aec42225a820dcfd0621ecccd7d29c05c66f8403
PolMrt <pol@marnette.be>
2017-05-15 18:07:22 +0200

    First repo

--- Touch Battle/ViewController.swift
+++ Touch Battle/ViewController.swift
@@ -9,6 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var Joueur2: UIButton!
+    @IBOutlet weak var Joueur1: UIButton!
+    
+    @IBAction func Joueur1Click(_ sender: UIButton) {
+        Joueur1.frame.size.height += 20
+        Joueur2.frame.size.height -= 20
+    }
+
+    @IBAction func Joueur2Click(_ sender: UIButton) {
+        Joueur2.frame.size.height += 20
+        Joueur1.frame.size.height -= 20
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
